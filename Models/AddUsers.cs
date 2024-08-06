7e1df9774e3543b0208b52b37ac77b8e8e557d45
st-dotnet <stdev.net@gmail.com>
2024-08-07 01:15:07 +0530

    inital commit

---
+++ Models/AddUsers.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebForm.Models
+{
+    public class AddUsers
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Age { get; set; }
+        public long PhoneNumber { get; set; }
+        public long AlternatePhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string Gender { get; set; }
+        public string MaritalStatus { get; set; }
+    }
+}
---
+++ Models/UpdateUser.cs
@@ -0,0 +1,17 @@
+﻿namespace WebForm.Models
+{
+    public class UpdateUser
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Age { get; set; }
+        public long PhoneNumber { get; set; }
+        public long AlternatePhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string Gender { get; set; }
+        public string MaritalStatus { get; set; }
+    }
+}
